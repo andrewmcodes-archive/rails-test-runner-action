f658961b05afb433cbd50a67be1125e8affb7aa5
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-13 20:06:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,6 @@ printenv
 cd $GITHUB_WORKSPACE
 gem update --system
 gem install bundler --no-document
+bundle install --jobs 4 --retry 3
+yarn install --frozen-lockfile
+bundle exec rails db:prepare && bundle exec rails test
